f958e9d1844eaeff69185ad401ae0358879a754f
ali84sama <ali.f.alshawaf@gmail.com>
2020-05-09 07:56:45 +0300

    Initial Commit

---
+++ Khatma Counter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Khatma Counter
+//
+//  Created by ALI on 5/9/20.
+//  Copyright © 2020 ALI. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
